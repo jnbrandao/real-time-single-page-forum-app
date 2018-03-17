fc8ceb787b37eefd5ee9b95b51ebcef44950a0ba
jnbrandao <jnbrandao@gmail.com>
2018-03-17 00:43:50 +0000

    Factory and seed.

--- database/factories/ModelCategoryFactory.php
+++ database/factories/ModelCategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'slug' => str_slug($faker->word)
     ];
 });
--- database/factories/ModelQuestionFactory.php
+++ database/factories/ModelQuestionFactory.php
@@ -4,6 +4,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Question::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'slug' => str_slug($faker->sentence),
+        'body' => $faker->text,
+        'category_id' => function () {
+            return \App\Model\Category::all()->random();
+        },
+        'user_id' => function () {
+            return \App\User::all()->random();
+        }
+
     ];
 });
